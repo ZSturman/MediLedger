db9b701d0e9a59e04451512b93ded055d7e55a18
Zachary Sturman <zasturman@gmail.com>
2025-03-24 13:34:05 -0700

    Initial Commit

---
+++ MedTracker/MedTrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  MedTrackerApp.swift
+//  MedTracker
+//
+//  Created by Zachary Sturman on 3/24/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MedTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MedTrackerTests/MedTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MedTrackerTests.swift
+//  MedTrackerTests
+//
+//  Created by Zachary Sturman on 3/24/25.
+//
+
+import Testing
+@testable import MedTracker
+
+struct MedTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
